ca8e785337eafac6a02ae6c8bea5f82d2f57b2dd
Laercio Diniz <laerciodiniz@gmail.com>
2019-11-21 00:10:43 -0300

    resize de imagens, configuracao socket.io, cors, BackEnd Finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const { DATABASE_URL } = require('./config/security');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
