e25a1ca8f6611812b205c60ac2d600e3c74b74ef
Laercio Diniz <laerciodiniz@gmail.com>
2019-11-28 23:35:10 -0300

    implementado rotas e navegacao

--- instarocket/src/index.js
+++ instarocket/src/index.js
@@ -1,26 +1,7 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
 
-export default class extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World</Text>
-      </View>
-    );
-  }
+import Routes from './routes';
+
+export default function App() {
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
