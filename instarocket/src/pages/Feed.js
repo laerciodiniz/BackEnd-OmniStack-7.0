831976f0e9c51c8bbc09d9dcb4f6f085970565a2
Laercio Diniz <laerciodiniz@gmail.com>
2019-11-29 23:38:47 -0300

    implementado realtime, Feed Finalizado.

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import api from '../services/api';
 import { Text, View, Image, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 
@@ -22,12 +23,32 @@ export default class Feed extends Component {
   }
 
   async componentDidMount() {
-   //this.registerToSocket();
+   this.registerToSocket();
 
     const response = await api.get('posts');
     console.log(response.data);
     this.setState({ feed: response.data })
 
+  }
+
+  registerToSocket = () => {
+    const socket = io('http://10.0.3.2:3333');
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    })
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      })
+    })
+  }
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
   }
 
   render() {
@@ -52,7 +73,7 @@ export default class Feed extends Component {
 
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
                     <Image source={like}/>
                   </TouchableOpacity>
                   <TouchableOpacity style={styles.action} onPress={() => {}}>
