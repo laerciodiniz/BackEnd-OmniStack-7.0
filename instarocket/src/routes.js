0a1b74a9f27cbf41b21c6be68276a2d14dbb42ce
Laercio Diniz <laerciodiniz@gmail.com>
2019-12-10 23:12:58 -0300

    InstaRocket finalizado

--- instarocket/src/routes.js
+++ instarocket/src/routes.js
@@ -13,7 +13,6 @@ export default createAppContainer(
         Feed,
         New,
     }, {
-        initialRouteName: 'New',
         defaultNavigationOptions: {
             headerTintColor: '#000',
             headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
